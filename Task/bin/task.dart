93e780e88bdbf74e5a72c4a1042daa171e5794ef
ahmed adel <A.Elazab18555@student.aast.edu>
2025-07-29 23:19:02 +0300

    Update task.dart

--- Task/bin/task.dart
+++ Task/bin/task.dart
@@ -47,3 +47,44 @@ void main() {
       return;
   }
 }
+
+
+
+
+
+import 'dart:io';
+
+int add(int num1, int num2) {
+  return num1 + num2;
+}
+
+int subtract(int num1, int num2) {
+  return num1 - num2;
+}
+
+int multiply(int num1, int num2) {
+  return num1 * num2;
+}
+
+double divide(int num1, int num2) {
+  return num1 / num2;
+}
+
+void mainFunction() {
+  stdout.write("Enter the first number: ");
+  int num1 = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter the second number: ");
+  int num2 = int.parse(stdin.readLineSync()!);
+
+  print("Nateg el Gam3: ${add(num1, num2)}");
+  print("Nateg el Tar7: ${subtract(num1, num2)}");
+  print("Nateg el Darb: ${multiply(num1, num2)}");
+  print("Nateg el Qesma: ${divide(num1, num2)}");
+}
+
+main() {
+  print("Program el 3mlyat el 7esabea");
+  mainFunction();
+}
+
